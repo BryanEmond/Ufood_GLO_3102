08b8e6dbda0bbcbcf2a153cdd28f286130dd4e17
Hugo Plante <hugo.plante.2@ulaval.ca>
2022-09-23 11:49:20 -0400

    navbar v1

---
+++ src/components/Navbar/Navigation.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav>
+    <div
+      class="flex flex-row space-x-2 p-2 justify-between border-b-2 items-bottom basis-4"
+    >
+      <div class="flex">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+        <div class="w-24 h-4 shrink-0">
+          <router-link to="/">
+            <img :src="ufoodSVG" />
+          </router-link>
+        </div>
+      </div>
+
+      <div class="flex-row space-x-2 justify-evenly grow hidden sm:flex">
+        <router-link to="/">Home</router-link>
+        <router-link to="/restaurant">Restaurant</router-link>
+        <router-link to="/user">User Profile</router-link>
+      </div>
+
+      <div class="shrink-0 sm:grow">
+        <search-bar></search-bar>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import SearchBar from "./SearchBar";
+export default {
+  name: "Navigation",
+  data() {
+    return {
+      ufoodSVG: require("../../assets/UFood.svg"),
+    };
+  },
+  components: {
+    "search-bar": SearchBar,
+  },
+};
+</script>
+
+<style></style>
