91a111aaeb26760d805ec9cb6e53d44f0c9421a6
baptiste <baptiste.renouf@epitech.com>
2022-10-03 02:01:07 +0200

    adding emits to close the side menu, adding a bit of css

--- src/components/Hamburger.vue
+++ src/components/Hamburger.vue
@@ -1,60 +1,63 @@
 <script>
 import PrimaryButton from "./PrimaryButton.vue";
-    export default {
-        name: "HamburgerMenu",
-        data(){
-            return{
-                isDisplay: false,
-                ufoodSVG: require("../assets/UFood.svg"),
-            }
-        },
-        components: {
-            "primary-button": PrimaryButton,
-        },
-        methods: {
-            changeIsDisplay: function(){
-                isDisplay = !isDisplay;
-                return this.isDisplay;
-            }
-        }
-    }
+export default {
+  name: "HamburgerMenu",
+  data() {
+    return {
+      isDisplay: false,
+      ufoodSVG: require("../assets/UFood.svg"),
+    };
+  },
+  components: {
+    "primary-button": PrimaryButton,
+  },
+  methods: {
+    changeIsDisplay: function () {
+      this.$emit("close-side-menu", true);
+      return;
+    },
+  },
+};
 </script>
-  
+
 <template>
-    <nav>
-        <div class="fixed top-0 left-0 h-screen flex flex-col space-y-5 p-2 justify-start border-r-2 bg-white">
-            <div class="flex flex-row justify-end">
-                <svg 
-                v-on:click="isDisplay = !isDisplay"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="cursor-pointer w-6 h-6 justify-end"
-                >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-                </svg>
-            </div>
-            <div class="w-24 h-4 shrink-0 al">
-                <router-link to="/">
-                    <img :src="ufoodSVG" />
-                </router-link>
-            </div>
-            <div class="flex flex-col space-y-5 justify-start grow pt-3">
-                <div class="flex flex-col space-y-2">
-                    <router-link to="/restaurant">Restaurant</router-link>
-                    <router-link to="/user">Profile</router-link>
-                </div>
-                <div class="flex flex-col space-y-2">
-                    <primary-button class="">Sign In</primary-button>
-                    <primary-button fill class="">Sign Up</primary-button>
-                </div>
-            </div>
+  <nav>
+    <div
+      id="burgerBtn"
+      class="fixed top-0 h-screen flex flex-col space-y-5 p-2 justify-start border-r-2 bg-white pl-3 w-3/5"
+    >
+      <div class="flex flex-row justify-between">
+        <div class="w-24 h-4 shrink-0">
+          <router-link to="/">
+            <img :src="ufoodSVG" />
+          </router-link>
         </div>
-    </nav>
+        <svg
+          v-on:click="changeIsDisplay()"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="cursor-pointer w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
+      <div class="flex flex-col space-y-5 justify-start grow pt-3">
+        <div class="flex flex-col space-y-2">
+          <router-link to="/restaurant">Restaurant</router-link>
+          <router-link to="/user">Profile</router-link>
+        </div>
+        <div class="flex flex-row justify-around">
+          <primary-button class="">Sign In</primary-button>
+          <primary-button fill class="">Sign Up</primary-button>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
