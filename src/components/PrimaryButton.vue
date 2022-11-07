6c925c09698364bc3cea401b8d571911c48e0c0a
Hugo Plante <hugo.plante.2@ulaval.ca>
2022-11-07 18:46:53 -0500

    ajout liste favoris et affichage de base

--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -3,7 +3,6 @@
     <button
       type="button"
       class="px-2 py-1 rounded-2xl border-solid border-2 border-sky-600 text-white"
-      v-bind:class="['fill' ? 'bg-sky-600' : 'bg-white']"
     >
       <slot></slot>
     </button>
---
+++ src/components/favorites/FavoritesList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div
+      class="p-4 mr-4 text-left rounded-3xl shadow mt-8 cursor-pointer w-44 h-32 hover:bg-slate-100"
+      @click="openModal"
+    >
+      <div class="flex flex-col h-full justify-around">
+        <div class="text-xl font-bold">{{ list.name }}</div>
+        <div class="justify-self-end">
+          {{ list.restaurants.length }} restaurants
+        </div>
+      </div>
+    </div>
+    <FavoriteListModal
+      :open="isOpen"
+      :list="list"
+      :closeCallback="closeModal"
+    ></FavoriteListModal>
+  </div>
+</template>
+
+<script>
+import FavoriteListModal from "./FavoriteListModal.vue";
+import { ref } from "vue";
+
+export default {
+  data() {
+    return {
+      isOpen: ref(false),
+    };
+  },
+  props: {
+    list: Object,
+    listUpdated: Function,
+  },
+  methods: {
+    openModal() {
+      this.isOpen = open;
+      console.log("open modal");
+    },
+
+    closeModal() {
+      this.isOpen = false;
+      console.log("close modal");
+      this.listUpdated();
+    },
+  },
+  components: {
+    FavoriteListModal,
+  },
+};
+</script>
