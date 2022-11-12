ef9316cd8e8d0d652a2796e77141c7d83a194ad8
BryanEmond <thunderghost22@outlook.com>
2022-11-12 14:49:08 -0500

    resolve warning

--- src/api/favoritesAPI.js
+++ src/api/favoritesAPI.js
@@ -1,103 +1,109 @@
 import { ENDPOINT_UNSECURE } from "./endpoint";
 
 export const getAllFavorites = async () => {
-    const response = await fetch(`${ENDPOINT_UNSECURE}/favorites`, {
-        method: 'GET',
-    });
-    const data = await response.json();
-    return data.items
-}
+  const response = await fetch(`${ENDPOINT_UNSECURE}/favorites`, {
+    method: "GET",
+  });
+  const data = await response.json();
+  return data.items;
+};
 
 export const getList = async (id) => {
-    const response = await fetch(`${ENDPOINT_UNSECURE}/favorites/${id}`, {
-        method: 'GET',
-    });
+  const response = await fetch(`${ENDPOINT_UNSECURE}/favorites/${id}`, {
+    method: "GET",
+  });
 
-    const data = await response.json();
-    return data.items
-}
+  const data = await response.json();
+  return data.items;
+};
 
 export const getListsFromUser = async (id) => {
-    const response = await fetch(`${ENDPOINT_UNSECURE}/users/${id}/favorites/`, {
-        method: 'GET',
-    });
+  const response = await fetch(`${ENDPOINT_UNSECURE}/users/${id}/favorites/`, {
+    method: "GET",
+  });
 
-    const data = await response.json();
-    return data.items
-}
+  const data = await response.json();
+  return data.items;
+};
 
 export const createList = async (name, owner) => {
-    const response = await fetch(`${ENDPOINT_UNSECURE}/favorites`, {
-        method: 'POST',
-        headers: { "Content-type": "application/json; charset=UTF-8" },
-        body: JSON.stringify({ name: name, owner: owner })
-    });
+  const response = await fetch(`${ENDPOINT_UNSECURE}/favorites`, {
+    method: "POST",
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+    body: JSON.stringify({ name: name, owner: owner }),
+  });
 
-    if (response.status != 201) {
-        console.log("Error while creating list");
-        return;
-    }
-    console.log("List created id: " + response.id);
-    console.log(response);
+  if (response.status != 201) {
+    console.log("Error while creating list");
+    return;
+  }
+  console.log("List created id: " + response.id);
+  console.log(response);
 
-    return response.data;
+  return response.data;
 };
 
 export const renameList = async (newName, owner, listId) => {
-    const response = await fetch(`${ENDPOINT_UNSECURE}/favorites/${listId}`, {
-        method: 'PUT',
-        headers: { "Content-type": "application/json; charset=UTF-8" },
-        body: JSON.stringify({ name: newName, owner: owner })
-    });
+  const response = await fetch(`${ENDPOINT_UNSECURE}/favorites/${listId}`, {
+    method: "PUT",
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+    body: JSON.stringify({ name: newName, owner: owner }),
+  });
 
-    if (response.status != 200) {
-        console.log("Error while renaming list");
-        return;
-    }
-    console.log("List renamed id: " + response.id);
+  if (response.status != 200) {
+    console.log("Error while renaming list");
+    return;
+  }
+  console.log("List renamed id: " + response.id);
 };
 
 export const deleteList = async (listId) => {
-    const response = await fetch(`${ENDPOINT_UNSECURE}/favorites/${listId}`, {
-        method: 'DELETE',
-        headers: { "Content-type": "application/json; charset=UTF-8" },
-    });
+  const response = await fetch(`${ENDPOINT_UNSECURE}/favorites/${listId}`, {
+    method: "DELETE",
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  });
 
-    if (response.status != 200) {
-        console.log("Error while deleting list");
-        return;
-    }
-    console.log("list deleted ");
+  if (response.status != 200) {
+    console.log("Error while deleting list");
+    return;
+  }
+  console.log("list deleted ");
 };
 
 export const addToList = async (listId, restaurantId) => {
-    const response = await fetch(`${ENDPOINT_UNSECURE}/favorites/${listId}/restaurants`, {
-        method: 'POST',
-        headers: { "Content-type": "application/json; charset=UTF-8" },
-        body: JSON.stringify({ id: restaurantId })
-    });
-
-    if (response.status != 200) {
-        console.log("Error while adding to list");
-        return;
+  const response = await fetch(
+    `${ENDPOINT_UNSECURE}/favorites/${listId}/restaurants`,
+    {
+      method: "POST",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      body: JSON.stringify({ id: restaurantId }),
     }
-    console.log("restaurant added");
-    console.log(data);
-    return response.data;
+  );
+
+  if (response.status != 200) {
+    console.log("Error while adding to list");
+    return;
+  }
+  console.log("restaurant added");
+  console.log(data);
+  return response.data;
 };
 
 export const removeFromList = async (listId, restaurantId) => {
-    const response = await fetch(`${ENDPOINT_UNSECURE}/favorites/${listId}/restaurants/${restaurantId}`, {
-        method: 'DELETE',
-        headers: { "Content-type": "application/json; charset=UTF-8" }
-    });
-
-    if (response.status != 200) {
-        console.log("Error while removing from list");
-        return;
+  const response = await fetch(
+    `${ENDPOINT_UNSECURE}/favorites/${listId}/restaurants/${restaurantId}`,
+    {
+      method: "DELETE",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
     }
-    console.log("restaurant removed");
-    console.log(data);
+  );
 
-    return response.data;
+  if (response.status != 200) {
+    console.log("Error while removing from list");
+    return;
+  }
+  console.log("restaurant removed");
+  console.log(data);
+
+  return response.data;
 };
