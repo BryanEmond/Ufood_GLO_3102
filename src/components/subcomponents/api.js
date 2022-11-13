93ffbae5c42693128e69f3aee2a92a75bda258c6
BryanEmond <thunderghost22@outlook.com>
2022-11-13 13:04:44 -0500

    Added visite on restaurants card in the home page

--- src/components/subcomponents/api.js
+++ src/components/subcomponents/api.js
@@ -1,26 +1,34 @@
+import { getInfo } from "../restaurant_script";
+
 const URL = "https://ufoodapi.herokuapp.com";
 
 export const fetchClosestRestaurants = async (position) => {
-  // const fetchRestaurants = await fetch(`${URL}/unsecure/restaurants?lon=${-71.297516}&lat=${46.780953}`);
+  // const fetchRestaurants = await fetch(
+  //   `${URL}/unsecure/restaurants?lon=${-71.297516}&lat=${46.780953}`
+  // );
   const fetchRestaurants = await fetch(
-    `${URL}/unsecure/restaurants?lon=${position.longitude}&lat=${position.latitude}`
+    `${URL}/unsecure/restaurants?lon=${-71.228446}&lat=${46.834502}`
   );
+  // const fetchRestaurants = await fetch(
+  //   `${URL}/unsecure/restaurants?lon=${position.longitude}&lat=${position.latitude}`
+  // );
   const json = await fetchRestaurants.json();
   return json.items;
 };
 export const fetchFavoriteRestaurants = async (uid) => {
   const fetchRestaurants = await fetch(
-    `${URL}/unsecure/users/:${uid}/favorites`
+    `${URL}/unsecure/users/${uid}/favorites`
   );
   const json = await fetchRestaurants.json();
-  return json.restaurants;
-};
-export const fetchVisitsRestaurants = async (uid) => {
-  const fetchRestaurants = await fetch(
-    `${URL}/unsecure/users/:${uid}/restaurants/visits`
-  );
-  const json = await fetchRestaurants.json();
-  return json.restaurants;
+  let favList = [];
+  for (let rest of json.items) {
+    if (rest.restaurants.length !== 0) {
+      for (let id of rest.restaurants) {
+        favList.push(await getInfo(id.id));
+      }
+    }
+  }
+  return favList;
 };
 export const fetchByRangePriceRestaurants = async (range) => {
   const fetchRestaurants = await fetch(
@@ -30,10 +38,24 @@ export const fetchByRangePriceRestaurants = async (range) => {
   return json.items;
 };
 export const fetchGenresRestaurants = async (cat) => {
-  // const fetchRestaurants = await fetch(`${URL}/unsecure/restaurants?lon=${-71.297516}&lat=${46.780953}`);
   const fetchRestaurants = await fetch(
     `${URL}/unsecure/restaurants?genres=${cat}`
   );
   const json = await fetchRestaurants.json();
   return json.items;
+};
+export const fetchVisitedRestaurants = async (uid) => {
+  const fetchRestaurants = await fetch(
+    `${URL}/unsecure/users/${uid}/restaurants/visits`
+  );
+  const json = await fetchRestaurants.json();
+  return json.items;
+};
+export const fetchVisitsRestaurants = async (uid) => {
+  const items = await fetchVisitedRestaurants(uid);
+  let visitedList = [];
+  for (let rest of items) {
+    visitedList.push(await getInfo(rest.restaurant_id));
+  }
+  return visitedList;
 };
