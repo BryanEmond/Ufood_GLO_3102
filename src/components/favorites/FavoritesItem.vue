230026685fafc11684420dfc7a065cebb3b95633
charles lohest <charles.lohest@student.uclouvain.be>
2022-11-13 21:51:25 -0500

    modal

--- src/components/favorites/FavoritesItem.vue
+++ src/components/favorites/FavoritesItem.vue
@@ -3,15 +3,31 @@
     <router-link v-bind:to="'restaurant?id=' + restoId">
       <p>{{ restaurantData.name }}</p>
     </router-link>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-2 mx-4 rounded"
+      @click="this.openModal"
+    >
+      View visit informations
+    </button>
     <button @click="removeRestaurant" class="text-red-600">X</button>
+    <VisitModalViewVue
+      :restaurantId="restoId"
+      :open="isOpen"
+      :closeCallback="closeModal"
+    >
+    </VisitModalViewVue>
   </div>
 </template>
 
 <script>
 import { getInfo } from "../restaurant_script";
 import { removeFromList } from "../../api/favoritesAPI";
+import VisitModalViewVue from "../VisitModalView.vue";
 export default {
   name: "FavoritesItem",
+  components: {
+    VisitModalViewVue,
+  },
   props: {
     restoId: String,
     listId: String,
@@ -19,6 +35,7 @@ export default {
   data() {
     return {
       restaurantData: {},
+      isOpen: false,
     };
   },
   beforeCreate: async function () {
@@ -31,6 +48,12 @@ export default {
       var el = document.getElementById(this.restoId);
       el.innerHTML = "";
       await removeFromList(this.listId, this.restoId);
+    },
+    openModal() {
+      this.isOpen = true;
+    },
+    closeModal() {
+      this.isOpen = false;
     },
   },
 };
