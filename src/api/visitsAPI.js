394ef9a80b3c635d046208f0c234485020d7f388
baptiste <baptiste.renouf@epitech.com>
2022-12-05 01:55:58 +0100

    fixing warnings and errors

--- src/api/visitsAPI.js
+++ src/api/visitsAPI.js
@@ -1,9 +1,11 @@
-async function getVisits() {
+import { ENDPOINT_UNSECURE } from "./endpoint";
+
+export const getVisits = async () => {
   //must change for 3rd delivery, instead getLocalStorage
   const userId = "636969b87bed3d6cd9563f4d";
 
   const response = await fetch(
-    `https://ufoodapi.herokuapp.com/unsecure/users/${userId}/restaurants/visits`,
+    `${ENDPOINT_UNSECURE}/users/${userId}/restaurants/visits`,
     {
       method: "GET",
       headers: { "Content-type": "application/json; charset=UTF-8" },
@@ -16,14 +18,14 @@ async function getVisits() {
   const data = await response.json();
 
   return data;
-}
+};
 
-async function getOneVisit(visitId) {
+export const getOneVisit = async (visitId) => {
   //must change for 3rd delivery, instead getLocalStorage
   const userId = "636969b87bed3d6cd9563f4d";
 
   const response = await fetch(
-    `https://ufoodapi.herokuapp.com/unsecure/users/${userId}/restaurants/visits/${visitId}`,
+    `${ENDPOINT_UNSECURE}/users/${userId}/restaurants/visits/${visitId}`,
     {
       method: "GET",
       headers: { "Content-type": "application/json; charset=UTF-8" },
@@ -36,14 +38,14 @@ async function getOneVisit(visitId) {
   const data = await response.json();
 
   return data;
-}
+};
 
-async function postVisit(restaurantId, comment, rating, date) {
+export const postVisit = async (restaurantId, comment, rating, date) => {
   //must change for 3rd delivery, instead getLocalStorage
   const userId = "636969b87bed3d6cd9563f4d";
 
   const response = await fetch(
-    `https://ufoodapi.herokuapp.com/unsecure/users/${userId}/restaurants/visits`,
+    `${ENDPOINT_UNSECURE}/users/${userId}/restaurants/visits`,
     {
       method: "POST",
       headers: { "Content-type": "application/json; charset=UTF-8" },
@@ -62,14 +64,14 @@ async function postVisit(restaurantId, comment, rating, date) {
   const data = await response.json();
 
   return data;
-}
+};
 
-async function getRestaurantVisits(restaurantId) {
+export const getRestaurantVisits = async (restaurantId) => {
   //must change for 3rd delivery, instead getLocalStorage
   const userId = "636969b87bed3d6cd9563f4d";
 
   const response = await fetch(
-    `https://ufoodapi.herokuapp.com/unsecure/users/${userId}/restaurants/${restaurantId}/visits`,
+    `${ENDPOINT_UNSECURE}/users/${userId}/restaurants/${restaurantId}/visits`,
     {
       method: "GET",
       headers: { "Content-type": "application/json; charset=UTF-8" },
@@ -82,12 +84,4 @@ async function getRestaurantVisits(restaurantId) {
   const data = await response.json();
 
   return data;
-}
-
-module.exports = {
-  getVisits,
-  getOneVisit,
-  postVisit,
-  getOneVisit,
-  getRestaurantVisits,
 };
